97cc6c5c6daf7408b0aa458ed5f2473e3c02ad1f
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-06-19 20:50:54 -0400

    runs queries if you have a keyfile

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
 import { IDriverAlias } from '@sqltools/types';
 
-const { displayName } = require('../package.json');
-
 /**
  * Aliases for yout driver. EG: PostgreSQL, PG, postgres can all resolve to your driver
  */
-export const DRIVER_ALIASES: IDriverAlias[] = [{ displayName: displayName, value: displayName }];
+export const DRIVER_ALIASES: IDriverAlias[] = [{ displayName: "BigQuery", value: "BigQuery" }];
